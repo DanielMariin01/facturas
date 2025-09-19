aa0284fa39fadfae3400ae13e31da9568e907f99
ANALISTA.TI3 - Daniel Stiven Marin <ANALISTA.TI3@CLINICACE.LOCAL>
2025-09-19 11:47:31 -0500

    first commit

---
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Providers\Filament;
+
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\AuthenticateSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Pages;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Widgets;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+
+class AdminPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->default()
+            ->id('admin')
+            ->path('admin')
+            ->login()
+              ->favicon(asset('imagenes/logo2.png'))
+             ->brandLogo(asset('imagenes/logo.png'))
+            ->colors([
+             'primary' => [
+    '50' => '#E0F7F7',
+    '100' => '#B3EAEA',
+    '200' => '#80DDDD',
+    '300' => '#4DCFCF',
+    '400' => '#26C2C2',
+    '500' => '#00B5B5', // color principal similar a #008080
+    '600' => '#009E9E',
+    '700' => '#008787',
+    '800' => '#006F6F',
+    '900' => '#005858',
+],
+
+            ])
+            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
+            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            ->pages([
+                Pages\Dashboard::class,
+            ])
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            ->widgets([
+                Widgets\AccountWidget::class,
+          
+             
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                Authenticate::class,
+            ]);
+    }
+}
---
+++ resources/views/filament/widgets/imagen-widget.blade.php
@@ -0,0 +1,10 @@
+<x-filament-widgets::widget>
+    <x-filament::section class="p-0">
+        <img 
+            src="{{ asset('imagenes/logo.png') }}" 
+            alt="Logo Clínica" 
+            class="w-full h-auto object-cover rounded-lg"
+        >
+    </x-filament::section>
+</x-filament-widgets::widget>
+
